1438c7f63e4f404b3117993c0c8ac6553ec05edc
Petr Javorik <jvr23@linuxmail.org>
2017-01-25 14:39:46 +0100

    initial commit

---
+++ bfx-api-cpp/console-wrapper.cpp
@@ -0,0 +1,44 @@
+// console-wrapper.cpp
+// Defines the entry point for the console application
+//
+
+
+#include <iostream>
+#include "BitfinexAPI.hpp"
+#include <fstream>
+
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+
+
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        cout << "usage: console-wrapper.cpp path/to/keyFile" << endl
+        << "first line is accessKey and second line is secretKey" << endl;
+        return -1;
+    }
+    ifstream ifs(argv[1]);
+    if (!ifs.is_open())
+    {
+        cout << "Can't open file: " << argv[1] << endl;
+        return -1;
+    }
+    else
+    {
+        string accessKey, secretKey;
+        getline(ifs, accessKey);
+        getline(ifs, secretKey);
+        BitfinexAPI bfxAPI(accessKey, secretKey);
+        string result1;
+        bfxAPI.getTicker(result1, "btcusd");
+        cout << result1 << endl;
+        ifs.close();
+        return 0;
+    }
+}
