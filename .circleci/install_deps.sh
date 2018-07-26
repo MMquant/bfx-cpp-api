26a88720fd1962298b4ab753f254da1eaaf04510
Maple Mapleson <maple@mmquant.net>
2018-07-26 12:03:18 +0200

    Update .circleci

--- .circleci/install_deps.sh
+++ .circleci/install_deps.sh
@@ -11,10 +11,11 @@ if ! [[ $HOME/custom_deps == *"$PATH"* ]]; then
     exit 1
 fi
 
-# CMake
+## CMake
 echo "Installing CMake..."
 curl -sS -o cmake.tar.gz https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
 echo "Extracting CMake..."
-tar -xf cmake.tar.gz
-ln -s cmake-3.12.0-Linux-x86_64/cmake cmake
+# delete archive so that it's not being cache "checksummed" by circleci
+tar -xf cmake.tar.gz && rm cmake.tar.gz
+ln -s cmake-3.12.0-Linux-x86_64/bin/cmake cmake
 echo "Done!"
