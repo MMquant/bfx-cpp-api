59b96a8d38fa17eab4c9f8dae74621e09dd094b9
Maple Mapleson <maple@mmquant.net>
2018-07-26 10:49:32 +0200

    install_deps.sh edit

--- .circleci/install_deps.sh
+++ .circleci/install_deps.sh
@@ -4,8 +4,12 @@
 mkdir $HOME/custom_deps
 export PATH=$PATH:$HOME/custom_deps
 cd $HOME/custom_deps
+echo "~/custom_deps directory successfully created"
 
 # CMake
-curl -o cmake.tar.gz https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
+echo "Installing CMake..."
+curl -sS -o cmake.tar.gz https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
+echo "Extracting CMake..."
 tar -xf cmake.tar.gz
 ln -s cmake-3.12.0-Linux-x86_64/cmake cmake
+echo "Done!"
